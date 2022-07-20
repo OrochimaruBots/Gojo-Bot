2a75712a4b565c60b710092ad9d9e5317b98fcfa
OrochimaruBotz <109642842+OrochimaruBotz@users.noreply.github.com>
2022-07-20 10:49:16 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,27 +12,27 @@ global.APIKeys = {
 }
 
 // setting 
-global.autoread = false // auto read pesan / message
-//global.autorecording = false //status auto merekam ( auto record )
-//global.autoketik = false //status auto mengetik (auto typing)
-//global.available = false //status online (online)
+global.autoread = true // auto read pesan / message
+//global.autorecording = true //status auto merekam ( auto record )
+//global.autoketik = true //status auto mengetik (auto typing)
+//global.available = true //status online (online)
 
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ManusiaDemonz" //nama kalian
-global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
-global.github = "https://zeeone-ofc.github.io/" //bebas
+global.botname = "YamatoBot" //namabot kalian
+global.ownername= "Yamato" //nama kalian
+global.myweb ="https://api.yamato.xyz" //bebas asal jan hapus
+global.youtube = "https://youtube.com/c/YamatoBotz" //bebas asal jan hapus
+global.github = "https://OrochimaruBotz.github.io/" //bebas
 global.email = "zeeoneofc@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.ownernomer = "6288804947282" // nomor wa kalian
 global.ownernomerr = "+6288804947282" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
-global.donasi = "./image/donasi.jpg" // foto donasi di folder image
+global.donasi = "./image/lol.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.owner = ["6288804947282","6288804947282","918156874290"] //ganti agar fitur owner bisa di gunakan
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ManusiaDemonz' //sticker wm ganti nama kalian
+global.owner = ["6288804947282","6288804947282","6288804947282"] //ganti agar fitur owner bisa di gunakan
+global.packname = '© YamatoBot' //sticker wm ubah
+global.author = 'By Yamato' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
@@ -42,7 +42,7 @@ global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutp
 global.prefa = ['']
 global.sp = '⭔'
 global.mess = {
-    sukses: '🤗Done, Oke Desu~',
+    sukses: 'DONE',
     admin: 'Perintah Ini Hanya Bisa Digunakan Oleh Admin !',
     botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin !',
     owner: 'Perintah Ini Hanya Bisa Digunakan Oleh Owner !',
